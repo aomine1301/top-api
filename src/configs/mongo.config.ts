88c34d8daffc32978bbcf8e8c9ee45c7863433d7
Программист <grooom1301@gmail.com>
2021-10-15 21:10:53 +0300

    add initial project

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,12 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
+	return {
+		uri: getMongoString(configService),
+		// ...getMongoOptions()
+	};
+};
+const getMongoString = (configService:ConfigService) => {
+
+};
